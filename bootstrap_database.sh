ac44470e27777946c57e80ed85c17222012d4faa
Bonifacio de Oliveira <bonifacio.segundo@gmail.com>
2017-07-16 15:37:09 -0300

    remove comando desnecessario

--- bootstrap_database.sh
+++ bootstrap_database.sh
@@ -4,5 +4,3 @@ mysql -u root -e "CREATE DATABASE folha_limpa"
 for sqlfile in $(ls hackfestdumps); do
     mysql -u root --password="" folha_limpa < hackfestdumps/$sqlfile
 done
-
-# mysql -u root --password="" folha_limpa < hackfestdumps/folha_limpa_cargos_municipios.sql
